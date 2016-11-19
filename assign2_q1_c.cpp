0228ee14d1deb7d4e2fcbaa3dcfad15388c978f1
rc <rohitchouhan07@gmail.com>
2016-11-19 22:27:40 +0530

    moar

---
+++ assign2_q1_c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int i;
+cout<<" Number "<<" Square "<<" Cube "<<endl;
+i=1;
+while (i<101)
+{
+cout<<"   "<<i<<"       "<<i*i<<"       "<<i*i*i<<endl;
+i++;
+}
+return 0;
+}
---
+++ assign2_q1_d.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int i;
+cout<<" Number "<<" Square "<<" Cube "<<endl;
+i=1;
+do
+{
+cout<<"   "<<i<<"       "<<i*i<<"       "<<i*i*i<<endl;
+i++;
+}
+while (i<101);
+return 0;
+}
+
