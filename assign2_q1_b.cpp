db0674769ea1bb9837fca5fbe34c00e75a1b599b
rc <rohitchouhan07@gmail.com>
2016-11-19 22:16:17 +0530

    derp fixes

--- assign2_q1_b.cpp
+++ assign2_q1_b.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-int x,y,z,i;
+int i;
 cout<<" Number "<<" Square "<<" Cube "<<endl;
 for (i=1;i<101;i++){
 cout<<"   "<<i<<"       "<<i*i<<"       "<<i*i*i<<endl;
