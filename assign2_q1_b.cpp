c258c5b8aa5988f5c42426c4f527f9a52b980378
rc <rohitchouhan07@gmail.com>
2016-11-19 22:13:06 +0530

    LEt the code go open source

---
+++ assign2_q1_b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int x,y,z,i;
+cout<<" Number "<<" Square "<<" Cube "<<endl;
+for (i=1;i<101;i++){
+cout<<"   "<<i<<"       "<<i*i<<"       "<<i*i*i<<endl;
+
+}
+return 0;
+}
